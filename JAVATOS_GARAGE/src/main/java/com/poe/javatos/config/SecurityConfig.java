496aba3553a1ed7a83f49fa422b20c1ca7ec7d9d
Pedrog38 <55433110+Pedrog38@users.noreply.github.com>
2019-09-23 10:00:36 +0200

    update version web mvc
    transformation du projet en mode mvc web

---
+++ JAVATOS_GARAGE/src/main/java/com/poe/javatos/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.poe.javatos.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.poe.javatos.springsecurity.service.UserService;
+import com.poe.javatos.springsecurity.service.security.AppAuthProvider;
+
+@Configuration
+@EnableWebSecurity
+@EnableWebMvc /**Gestion du MVC Web pour les WAR et les JSP **/
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer{
+	@Autowired
+    UserService userDetailsService;
+
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf()
+                .disable()
+                .exceptionHandling()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/logout").permitAll()
+                .antMatchers("/app/**").permitAll()
+                .anyRequest().permitAll();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+    /**
+     * Configuration des chemins par défaut pour le menu et le formulaire
+     * de connexion.
+     */
+       registry.addViewController("/").setViewName("menu");
+       registry.addViewController("/login").setViewName("secure/login");
+    }
+   
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+    	/**
+    	 * Configuration très simple du dossier des JSP et
+    	 * de la résolution des noms passés par les controller
+    	 */
+    	// TODO Auto-generated method stub
+    	 registry.jsp("/WEB-INF/jsp/", ".jsp");
+    	WebMvcConfigurer.super.configureViewResolvers(registry);
+    }
+     
+    @Bean
+    public AuthenticationProvider getProvider() {
+
+        AppAuthProvider provider = new AppAuthProvider();
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+
+    }
+}
--- JAVATOS_GARAGE/src/main/java/com/poe/javatos/springsecurity/service/UserService.java
+++ JAVATOS_GARAGE/src/main/java/com/poe/javatos/springsecurity/service/UserService.java
@@ -1,26 +1,35 @@
 package com.poe.javatos.springsecurity.service;
 
+import com.poe.javatos.springsecurity.core.model.User;
+import com.poe.javatos.springsecurity.repo.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.poe.javatos.springsecurity.repo.UserRepository;
+import java.util.Objects;
 
 @Service
-public class UserService implements UserDetailsService {
-	
-	private final UserRepository userRepository;
-	
-	@Autowired
-	public UserService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
-	}
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Objects.requireNonNull(username);
+        User user = userRepository.findUserWithName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return user;
+    }
 
 }
