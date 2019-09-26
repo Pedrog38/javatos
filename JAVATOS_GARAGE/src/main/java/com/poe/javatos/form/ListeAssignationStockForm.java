291d71d5456a78e9a74b6ede20c8f6b22d271138
Adele-BERTRAND-DALECHAMPS <adele.bertrand.dalechamps@gmail.com>
2019-09-26 10:55:43 +0200

    assignation [controller+jsp] OK

--- JAVATOS_GARAGE/src/main/java/com/poe/javatos/form/ListeAssignationStockForm.java
+++ JAVATOS_GARAGE/src/main/java/com/poe/javatos/form/ListeAssignationStockForm.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.lang.NonNull;
+
 public class ListeAssignationStockForm 
 {
 
 	@Valid
 	private List<AssignationStockForm> listAss;
+	
+	private Integer index;
 
 	public List<AssignationStockForm> getListAss() 
 	{
@@ -18,6 +22,14 @@ public class ListeAssignationStockForm
 	public void setListAss(List<AssignationStockForm> listAss) 
 	{
 		this.listAss = listAss;
+	}
+
+	public Integer getIndex() {
+		return index;
+	}
+
+	public void setIndex(Integer index) {
+		this.index = index;
 	}
 	
 	
