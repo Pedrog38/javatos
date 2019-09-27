bca76d872f47e0733cf5f644acbacb5701932032
20Shadows <55386179+20Shadows@users.noreply.github.com>
2019-09-27 17:18:55 +0200

    commit login menu direct

--- JAVATOS_GARAGE/src/main/java/com/poe/javatos/controller/MenuController.java
+++ JAVATOS_GARAGE/src/main/java/com/poe/javatos/controller/MenuController.java
@@ -1,33 +1,50 @@
 package com.poe.javatos.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/Menu")
+@RequestMapping(value = "/MenuController")
 public class MenuController {
 	
-	@GetMapping(value ="A")
+	@GetMapping
 	public String afficherMenuAdmin (ModelMap model) 
 	{
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		
-		return "menuA";
+		boolean userIsAdmin = authentication.getAuthorities().stream()
+		          .anyMatch(r -> r.getAuthority().equals("Admin"));
+		
+		boolean userIsCom = authentication.getAuthorities().stream()
+		          .anyMatch(r -> r.getAuthority().equals("Commercial"));
+		
+		if(userIsAdmin) {
+		return "menuA";}
+		
+		if(userIsCom) {
+			return "menuC";}
+				else return "menuM";}
+					
+		
 	}
 	
-	@GetMapping(value ="C")
-	public String afficherMenuCommercial (ModelMap model) 
-	{
-		
-		return "menuC";
-	}
-	
-	@GetMapping(value ="M")
-	public String afficherMenuMagasinier (ModelMap model) 
-	{
-		
-		return "menuM";
-	}
+//	@GetMapping(value ="C")
+//	public String afficherMenuCommercial (ModelMap model) 
+//	{
+//		
+//		return "menuC";
+//	}
+//	
+//	@GetMapping(value ="M")
+//	public String afficherMenuMagasinier (ModelMap model) 
+//	{
+//		
+//		return "menuM";
+//	}
 
-}
+
+
